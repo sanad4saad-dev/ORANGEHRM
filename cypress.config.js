b769da5c055cd538e9775a582d86e883b62fd92d
sanad4saad-dev <sanad4saad@gmail.com>
2026-01-17 19:23:09 +0300

    backup

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+
+  vifeo:true, // for recording videos of test runs
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+
+    testIsolation: false
+  },
+});
